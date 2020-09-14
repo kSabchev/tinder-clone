7217aa3654b43e19272ef11a07e3fef93474f40d
kSabchev <kaloyansabchev@gmail.com>
2020-09-14 23:11:05 +0300

    hardcoded ui done

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import TinderCards from './TinderCards';
+import SwipeButtons from './SwipeButtons';
 
 function App() {
   return (
     <div className="app">
       <Header />
-      {/* TinderCards */}
+      <TinderCards />
+      <SwipeButtons /> 
       {/* SwipeButtons */}
     </div>
   );
